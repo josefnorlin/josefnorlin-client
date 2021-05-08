9cb5b10cd41d4a3659545b3d3a1b51d70e09ed34
Josef Norlin <Josef.Norlin@postkodlotteriet.se>
2021-05-08 09:28:20 +0200

    Changed desc to description

--- src/views/LandingPage/Sections/WorkSection.js
+++ src/views/LandingPage/Sections/WorkSection.js
@@ -33,7 +33,7 @@ export default function WorkSection() {
         {
           name,
           email,
-          desc: description,
+          description,
         }
       )
       .then((res) => {
